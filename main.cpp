f06981857f112bea70227a067db02f50e088fcf0
Cieran O'Neill <cieran999@gmail.com>
2022-11-27 17:30:51 +0000

    Audio: added mute and play icon for audio

--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@ clib::string* Gàidhlig;
 clib::string* Berula;
 clib::string page;
 clib::string sizeStr;
+Sound* sounds;
+Texture2D play;
+Texture2D mute;
+
+void loadImages() {
+    play = LoadTexture("images/play.png");
+    mute = LoadTexture("images/mute.png");
+}
 
 clib::string to_string(int n, int base) {
 
@@ -28,17 +36,28 @@ clib::string to_string(int n, int base) {
     return s;
 }
 
+void loadAudio() {
+    sounds = (Sound*)malloc(sizeof(Sound)*size);
+    for(int i = -1; i < size; i++) {
+        clib::string s = clib::string("audio/") + to_string(i,10) + clib::string(".mp3");
+        if(clib::FileExists(s)) {
+            sounds[i] = LoadSound(s);
+        } else {
+            std::cout << s << " is not found!" << std::endl;
+        }
+    }
+}
+
+
 void loadWords() {
     clib::string rawGàidhlig = clib::FileReadAllText("gàidhlig.txt");
     size = rawGàidhlig.countOccur('\n');
     Gàidhlig = rawGàidhlig.split('\n',size);
     Berula = clib::FileReadAllText("english.txt").split('\n',size);
     sizeStr = clib::string("/") + to_string(size,10);
-    std::cout << sizeStr << std::endl;
     page = to_string(current+1,10);
-    std::cout << page << std::endl;
     page = page + sizeStr;
-    std::cout << page << std::endl;
+
 }
 
 void moveBy(int count) {
@@ -56,7 +75,13 @@ int main(void)
     const int screenWidth = 600;
     const int screenHeight = 350;
     InitWindow(screenWidth, screenHeight, "Flashcards");
+    loadImages();
+    InitAudioDevice();
+    loadAudio();
     SetTargetFPS(60);
+    Vector2 pos;
+    pos.x = 475;
+    pos.y = 245;
 
     // Main game loop
     while (!WindowShouldClose())
@@ -67,8 +92,10 @@ int main(void)
             DrawText(Gàidhlig[current], (screenWidth/2)-(MeasureText(Gàidhlig[current],TXTSIZE_G)/2), 50, TXTSIZE_G, BLACK);
             DrawText(Berula[current], (screenWidth/2)-(MeasureText(Berula[current],TXTSIZE_B)/2), 120, TXTSIZE_B, BLACK);
             DrawText(page, 0,300,50, BLACK);
+            DrawTextureEx((sounds[current].frameCount > 0)? play : mute, pos, 0, 0.2, WHITE);
             if(IsKeyPressed(KEY_RIGHT)) {moveBy(1);}
             if(IsKeyPressed(KEY_LEFT)) {moveBy(-1);}
+            if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) { PlaySound(sounds[current]); }
 
         EndDrawing();
     }
