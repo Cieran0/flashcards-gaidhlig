c9b82847f6a1da0d53a5bf5b6a20db5b33a5ae2e
Cieran O'Neill <cieran999@gmail.com>
2022-11-27 17:47:34 +0000

    Audio: updated getAudio.sh script Now the script trims the first second of the audio clip

--- getAudio.sh
+++ getAudio.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 i=0
+rm audio/*.mp3
 while read link; do
     if [ -z "$link" ]
     then
         echo "Line $(($i+1)) empty"
     else
         echo "Link found in line $(($i+1)), downloading..."
-        curl -o audio/$i.mp3 $link &> /dev/null
+        curl -o audio/$i.u.mp3 $link &> /dev/null
     fi
     i=$(($i+1))
 done < audio.txt
+
+for ((x = 0 ; x < i ; x++)); do
+    ffmpeg -ss 1 -i audio/$x.u.mp3 -c copy audio/$x.mp3 &> /dev/null
+    rm audio/$x.u.mp3 &> /dev/null
+done
